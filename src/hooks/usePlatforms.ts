6ffa169f0cf4f2575d4fd659448ba90964358317
Paul Martinez <paulmartinez.career@gmail.com>
2025-04-01 09:26:07 -0700

    Implement react query for fetching games.

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,8 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
-import apiClient from "../services/api-client";
-import { FetchResponse } from "./useData";
+import apiClient, { FetchResponse } from "../services/api-client";
 import platforms from "../data/platforms";
-
 interface Platform {
   id: number;
   name: string;
